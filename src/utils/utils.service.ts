c8e271fd41a47d1ab417e98750ed3ce51e4a9049
Ông Thanh Bình <94289551+binhotvn@users.noreply.github.com>
2023-09-04 19:01:04 +0700

    init

---
+++ src/utils/utils.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import { sha512 } from 'js-sha512';
+import { User } from 'src/users/entities/user.entity';
+import { v4 as uuidv4 } from 'uuid';
+const NodeRSA = require('node-rsa');
+const KEY_DIR = `${process.cwd()}/private_key`;
+
+
+export const PRIVATE_KEY =  () => { 
+  const PRIVATE_KEY_FUNCTION = NodeRSA( fs.readFileSync(`${KEY_DIR}/key_for_jwt`))
+  return PRIVATE_KEY_FUNCTION.exportKey('private')
+};
+export const PUBLIC_KEY =  () => { 
+  const PRIVATE_KEY_FUNCTION = NodeRSA( fs.readFileSync(`${KEY_DIR}/key_for_jwt`))
+  return PRIVATE_KEY_FUNCTION.exportKey('public')
+};
+export const UNSAFE_ENTITIES_USER = ['token', 'password'];
+
+@Injectable()
+export class UtilsService {
+  randomToken(length = 10) {
+    const randomUUID = uuidv4();
+    return sha512(
+      sha512(randomUUID + randomUUID + randomUUID).substring(0, length * 3),
+    ).substring(0, 10);
+  }
+
+  async returnSafeUser(user: User): Promise<User> {
+    UNSAFE_ENTITIES_USER.forEach((entities) => {
+      user[entities] = undefined;
+      delete user[entities];
+    });
+    return user;
+  }
+}
